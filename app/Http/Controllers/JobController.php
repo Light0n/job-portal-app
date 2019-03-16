87cb02280ad66c0dcc961aab5414ed895b6febcd
nvdhau <nvdhau@gmail.com>
2019-03-16 15:56:49 -0700

    build query, combine result to JSON of all open jobs, and pass to front end

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -10,6 +10,27 @@ class JobController extends Controller
     public function index(){
         //show job that have status 'open'
         $jobs = DB::select("select * from job where status='open'");
+        foreach ($jobs as &$job) {//pass $job by reference
+            //hide attributes
+            unset($job->updated_at);
+            unset($job->jobseeker_id);
+            unset($job->jobseeker_status);
+            unset($job->employer_status);
+            unset($job->jobseeker_review_id);
+            unset($job->employer_review_id);
+            //get array of required skills
+            $job->required_skills = DB::select("select s.id, s.skill_name
+                from job as j join job_required_skill as jrs 
+                    on j.id = jrs.job_id 
+                    join skill as s 
+                    on jrs.skill_id = s.id 
+                    where j.id=".$job->id);
+            //get employer info
+            $job->employer_info = DB::select('select 
+                total_employer_reviews, employeer_avg_rate
+                from user where id = '.$job->employer_id);
+        }
+        unset($job);//destroy reference variable
         return view("jobs", compact("jobs"));
         // return dd(DB::select("select * from job where status='open'"));
     }
--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -5,16 +5,12 @@
     <div class="row mt-5">
         <div class="col-md-8 offset-md-2">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Jobs</div>
+                    <pre id="content">
 
+                    </pre>
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+                    
                 </div>
             </div>
         </div>
@@ -28,11 +24,10 @@
 @section('page-js-script')
 <script type="text/javascript">
     $(document).ready(function() {
-        // alert("Settings page was loaded");
-        // alert('jquery ver.' + $.fn.jquery)
         var jobs = {!! json_encode($jobs) !!};
-        console.log(jobs);
-        $("#content").text(JSON.stringify(jobs));
+        // console.log(jobs);
+        //print JSON to page
+        $("#content").html(JSON.stringify(jobs, undefined, 4));
     });
 </script>
 @stop
